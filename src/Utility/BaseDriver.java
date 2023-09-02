96ea75101c70f34f862aeb1852d9e3ff52f42f33
iremtorun <irmtorun@gmail.com>
2023-09-03 00:11:40 +0300

    base driver düzeltildi.

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -30,24 +30,10 @@ public class BaseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  // 20 sn mühlet: elementi bulma mühleti
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        loginTesti();
+
     }
 
-    public void loginTesti(){
-        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
-        MyFunc.Bekle(2);
 
-        WebElement inputEmail = driver.findElement(By.id("input-email"));
-        inputEmail.sendKeys("testng1@gmail.com");
-
-        WebElement inputpassword = driver.findElement(By.id("input-password"));
-        inputpassword.sendKeys("123qweasd");
-
-        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
-        loginBtn.click();
-
-        Assert.assertTrue(driver.getTitle().equals("My Account"));
-    }
 
     @AfterClass
     public void bitisIslemleri(){ // tearDown
