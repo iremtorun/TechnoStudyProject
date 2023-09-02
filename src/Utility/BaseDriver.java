4ae5a4637be18ff7c20890ac5785a880e35126a8
iremtorun <irmtorun@gmail.com>
2023-09-02 12:11:02 +0300

    Base driver ayarlandı

---
+++ src/Utility/BaseDriver.java
@@ -0,0 +1,44 @@
+package Utility;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseDriver {
+
+    public static WebDriver driver; // SingletonDriver method
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void baslangicIslemleri(){
+        Logger logger= Logger.getLogger(""); // output yapılan logları al.
+        logger.setLevel(Level.SEVERE); // sadece ERROR ları göster
+
+        driver = new ChromeDriver(); // jenkins deyken : sen head olmadan yani hafızada çalış
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  // 20 sn mühlet: elementi bulma mühleti
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+
+    }
+
+
+
+    @AfterClass
+    public void bitisIslemleri(){ // tearDown
+        MyFunc.Bekle(3);
+        driver.quit();
+    }
+
+}
---
+++ src/Utility/MyFunc.java
@@ -0,0 +1,23 @@
+package Utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class MyFunc {
+
+    public static void Bekle(int sn){
+
+        try {
+            Thread.sleep(sn*1000l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    // Çoğu yerde aynı mesaj doğrulaması kullanıldığı için bu bölümü
+    // ortak fonksiyonların yer aldığı MyFunc a taşındı.
+
+
+}
