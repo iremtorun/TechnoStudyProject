01b19e47e1fe7c82a0626c5013659be7186d4063
iremtorun <irmtorun@gmail.com>
2023-09-03 00:16:27 +0300

    base driver düzeltildi.

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -20,8 +20,8 @@ public class BaseDriver {
     public static WebDriverWait wait;
 
     @BeforeClass
-    public void baslangicIslemleri(){
-        Logger logger= Logger.getLogger(""); // output yapılan logları al.
+    public void baslangicIslemleri() {
+        Logger logger = Logger.getLogger(""); // output yapılan logları al.
         logger.setLevel(Level.SEVERE); // sadece ERROR ları göster
 
         driver = new ChromeDriver(); // jenkins deyken : sen head olmadan yani hafızada çalış
@@ -30,29 +30,12 @@ public class BaseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  // 20 sn mühlet: elementi bulma mühleti
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        loginTesti();
     }
+        @AfterClass
+        public void bitisIslemleri () { // tearDown
+            MyFunc.Bekle(3);
+            driver.quit();
+        }
 
-    public void loginTesti(){
-        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
-        MyFunc.Bekle(2);
 
-        WebElement inputEmail = driver.findElement(By.id("input-email"));
-        inputEmail.sendKeys("testng1@gmail.com");
-
-        WebElement inputpassword = driver.findElement(By.id("input-password"));
-        inputpassword.sendKeys("123qweasd");
-
-        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
-        loginBtn.click();
-
-        Assert.assertTrue(driver.getTitle().equals("My Account"));
     }
-
-    @AfterClass
-    public void bitisIslemleri(){ // tearDown
-        MyFunc.Bekle(5);
-        driver.quit();
-    }
-
-}
